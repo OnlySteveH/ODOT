b9b9add51a4a4de137990808d4a733cd8bec5123
OnlySteveH <steve.hunter@bigdogconsultants.co.uk>
2015-03-30 21:13:14 +0100

    General update

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150330151920) do
+ActiveRecord::Schema.define(version: 20150330152334) do
 
   create_table "todo_lists", force: :cascade do |t|
     t.string   "title"
-    t.string   "string"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
