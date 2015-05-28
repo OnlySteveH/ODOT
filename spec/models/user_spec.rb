18758a342a292a9a0bee8e9aca05d0d71b8338db
OnlySteveH <steve.hunter@bigdogconsultants.co.uk>
2015-05-28 15:25:41 +0100

    Added some user validation surrounding email format

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,8 +10,42 @@ describe User do
 	}
   context "validations" do
   	let(:user) { User.new(valid_attributes) }
+
+  	before do 
+			User.create(valid_attributes)
+		end
+
+
   	it "requires an email" do
   		expect(user).to validate_presence_of(:email)
   	end
+
+  	it "requires a unique email" do
+  		expect(user).to validate_uniqueness_of(:email)
+  	end
+
+  	it "requires a unique email (case insensitive)" do 
+  		user.email = "ONLYSTEVEH@ME.COM"
+  		expect(user).to validate_uniqueness_of(:email)
+  	end
+
   end
+
+  describe "#downcase_email" do
+  	it "makes the email attribute lower case" do
+  		user = User.new(valid_attributes.merge(email: "ONLYSTEVEH@ME.COM"))
+  		expect { user.downcase_email }.to change{ user.email }.
+  			from("ONLYSTEVEH@ME.COM").
+  			to("onlysteveh@me.com")
+  	end
+
+  	it "downcase an email before saving" do
+  		user = User.new(valid_attributes)
+  		user.email = "MIKE@TEAMTREEHOUSE.COM"
+  		expect(user.save).to be_truthy
+  		expect(user.email).to eq("mike@teamtreehouse.com")
+  	end
+
+  end
+
 end
